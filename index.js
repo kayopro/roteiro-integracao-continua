969374b6fc3e39bd58fe01681f6482f23beb8b9b
José Kayo <kayokj@gmail.com>
2024-02-08 17:57:05 -0300

    Commit 6 - Fatura em HTML

--- index.js
+++ index.js
@@ -1,77 +1,26 @@
-//"Commit 5 - Move Function"
+// "Commit 6 - Fatura em HTML"
 
-function formatarMoeda(valor) {
-  return new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-    minimumFractionDigits: 2,
-  }).format(valor / 100);
-}
-
-function getPeca(pecas, apre) {
-  return pecas[apre.id];
-}
-
-function calcularCredito(pecas, apre) {
-  let creditos = 0;
-  creditos += Math.max(apre.audiencia - 30, 0);
-  if (getPeca(pecas, apre).tipo === "comedia")
-    creditos += Math.floor(apre.audiencia / 5);
-  return creditos;
-}
-
-function calcularTotalCreditos(pecas, apresentacoes) {
-  let creditos = 0;
-  for (let apre of apresentacoes) {
-    creditos += calcularCredito(pecas, apre);
-  }
-  return creditos;
-}
-
-function calcularTotalApresentacao(pecas, apre) {
-  let total = 0;
-  const peca = getPeca(pecas, apre);
-  switch (peca.tipo) {
-    case "tragedia":
-      total = 40000;
-      if (apre.audiencia < 30) {
-        total += 1000 * (apre.audiencia - 30);
-      }
-      break;
-    case "comedia":
-      total = 30000;
-      if (apre.audiencia < 20) {
-        total += 10000 + 500 * (apre.audiencia - 20);
-      }
-      total += 300 * apre.audiencia;
-      break;
-    default:
-      throw new Error(`Tipo de peça desconhecido: ${peca.tipo}`);
-  }
-  return total;
-}
-
-function calcularTotalFatura(pecas, apresentacoes) {
-  let total = 0;
-  for (let apre of apresentacoes) {
-    total += calcularTotalApresentacao(pecas, apre);
-  }
-  return total;
-}
-
-function gerarFaturaStr(fatura, pecas) {
-  let faturaStr = `Fatura ${fatura.cliente}\n`;
+function gerarFaturaHTML(fatura, pecas) {
+  let faturaHTML = `
+<html>
+<p> Fatura ${fatura.cliente} </p>
+<ul>
+`;
   for (let apre of fatura.apresentacoes) {
-    faturaStr += `  ${getPeca(pecas, apre).nome}: ${formatarMoeda(
+    const peca = getPeca(pecas, apre);
+    faturaHTML += `<li> ${peca.nome}: ${formatarMoeda(
       calcularTotalApresentacao(pecas, apre)
-    )} (${apre.audiencia} assentos)\n`;
+    )} (${apre.audiencia} assentos) </li>\n`;
   }
-  faturaStr += `Valor total: ${formatarMoeda(
+  faturaHTML += `</ul>
+<p> Valor total: ${formatarMoeda(
     calcularTotalFatura(pecas, fatura.apresentacoes)
-  )}\n`;
-  faturaStr += `Créditos acumulados: ${calcularTotalCreditos(
+  )} </p>
+<p> Créditos acumulados: ${calcularTotalCreditos(
     pecas,
     fatura.apresentacoes
-  )} \n`;
-  return faturaStr;
+  )} </p>
+</html>
+`;
+  return faturaHTML;
 }
