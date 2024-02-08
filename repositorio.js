c4ed8bedaf334457102292fe6fbc56c6dc0d1a1b
DiogoGabriel-ADS <153551570+DiogoGabriel-ADS@users.noreply.github.com>
2024-02-08 20:00:05 -0300

    Update repositorio.js

--- repositorio.js
+++ repositorio.js
@@ -1,11 +1,1 @@
-const { readFileSync } = require("fs");
 
-module.exports = class Repositorio {
-  constructor() {
-    this.pecas = JSON.parse(readFileSync("./pecas.json"));
-  }
-
-  getPeca(apre) {
-    return this.pecas[apre.id];
-  }
-};
